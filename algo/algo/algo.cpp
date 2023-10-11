c3a3f05c701e0b96407811d7d75fe5504ba2a616
Berside <144826358+Berside@users.noreply.github.com>
2023-10-11 19:35:23 +0300

    v3
    +bubblesort
    -MegreSort
    fix

--- algo/algo/algo.cpp
+++ algo/algo/algo.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-int SelectionSort(int a[], int N) {
+void SelectionSort(int a[], int N) {
     int n = N;
     for (int j = (n - 1); j >= 2; j--) {
         int max_index = j;
@@ -19,47 +19,23 @@ int SelectionSort(int a[], int N) {
     }
 }
 
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
 bool checkArray(int arr[], int N) {
     for (int i = 0; i < N - 1; i++) {
         if (arr[i] > arr[i + 1]) return false;
     }
     return true;
-}
-
-int log2RoundUp(int N) {
-    int i = 0;
-    while (pow(2, i) < N) {
-        i++;
-    }
-    return i;
-}
-
-void BatchersMergeSort(int arr[], int N) {
-    int t = log2RoundUp(N);
-    int p0 = pow(2, t - 1);
-    int p = p0;
-
-    while (p > 0) {
-        int q = p0, r = 0, d = p;
-
-        while (r == 0 || q != p) {
-            if (r != 0)
-            {
-                d = q - p;
-                q >>= 1;
-            }
-
-            for (int i = 0; i < N - d; i++) {
-                if (((i & p) == r) && arr[i] > arr[i + d]) {
-                    swap(arr[i], arr[i + d]);
-                }
-            }
-
-            r = p;
-        }
-
-        p = p / 2;
-    }
 }
 
 int compare(const void* x1, const void* x2)   // функция сравнения элементов массива
@@ -70,12 +46,12 @@ int compare(const void* x1, const void* x2)   // функция сравнени
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    const int N = 5000;
+    const int N = 16;
     int a[N] = {};
     int c [N] = {};
     int n;
     int bab[N] = {};
-    ifstream input("d5000.txt");
+    ifstream input("d16.txt");
     for (int i = 0; i < N; i++)
     {
         input >> n;
@@ -84,7 +60,6 @@ int main()
         bab[i] = n;
     }
     input.close();
-
     auto start = chrono::steady_clock::now();
     qsort(c,N, sizeof(int),compare);
     auto end = chrono::steady_clock::now();
@@ -94,7 +69,7 @@ int main()
     auto end1 = chrono::steady_clock::now();
 
     auto start2 = chrono::steady_clock::now();
-    BatchersMergeSort(bab, N);
+    bubbleSort(bab, N);
     auto end2 = chrono::steady_clock::now();
 
     long duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
@@ -104,7 +79,7 @@ int main()
     cout << endl << "Время сортировки SelectionSort:" << " " << duration1 << endl;
 
     long duration2 = chrono::duration_cast<chrono::microseconds>(end2 - start2).count(); 
-    cout << endl << "Время сортировки  BatchersMergeSort:" << " " << duration2 << endl;
+    cout << endl << "Время сортировки  Bubblesort:" << " " << duration2 << endl;
 
     ofstream out("Sorted.txt");
     if (out.is_open()) {
